272d66062324b60a5e74d04f299daa03f8ccd313
sauron <dp20007@ues.edu.sv>
2026-03-24 21:25:23 -0600

    TI-10 Inicializacion de las clases para REST

---
+++ src/main/java/sv/edu/ues/occ/ingenieria/web/dar_tpi135_ingresoues/core/boundary/rest_server/JornadaAulaAspiranteResultadoResource.java
@@ -0,0 +1,43 @@
+package sv.edu.ues.occ.ingenieria.web.dar_tpi135_ingresoues.core.boundary.rest_server;
+
+import jakarta.inject.Inject;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import sv.edu.ues.occ.ingenieria.web.dar_tpi135_ingresoues.core.control.JornadaAulaAspiranteDAO;
+import sv.edu.ues.occ.ingenieria.web.dar_tpi135_ingresoues.core.control.JornadaAulaAspiranteResultadoDAO;
+
+import java.io.Serializable;
+
+//modificar el path a conveniencia de la logica que debe seguir
+@Path("jornadaAulaAspirante/{idJornadaAulaAspirante}/resultado")
+public class JornadaAulaAspiranteResultadoResource implements Serializable {
+    @Inject
+    JornadaAulaAspiranteResultadoDAO jornadaAulaAspiranteResultadoDAO;
+
+    @Inject
+    JornadaAulaAspiranteDAO jornadaAulaAspiranteDAO;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findRange(
+            @Min(0)
+            @DefaultValue("0")
+            @QueryParam("first")
+            int first,
+            @Max(10)
+            @DefaultValue("10")
+            @QueryParam("max")
+            int max,
+            @PathParam("idJornadaAulaAspirante") Long idJornadaAulaAspirante
+    ) {
+        //Desarrollar con con el flujo de TDD
+        //validacion de parametros
+        //captura de excepciones
+        //logica
+
+        return Response.status(422).header("Missing-parameter", "first,max").build();
+    }
+}
